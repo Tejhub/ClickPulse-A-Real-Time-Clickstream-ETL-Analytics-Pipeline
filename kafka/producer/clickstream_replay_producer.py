9e1c2c9f72492a88124239b1529c8ae9b8b33b36
Tejas Gurav <59908025+Tejhub@users.noreply.github.com>
2026-02-24 14:39:00 +0530

    Update clickstream_replay_producer.py

--- kafka/producer/clickstream_replay_producer.py
+++ kafka/producer/clickstream_replay_producer.py
@@ -1,32 +1,41 @@
-
-import time, json
+import time
+import json
 import pandas as pd
 from kafka import KafkaProducer
 
+KAFKA_BROKERS = ["localhost:9092", "localhost:9093", "localhost:9094"]
+TOPIC = "clickpulse_realtime_event"
+FILE_PATH = "../../data/raw/ecommerce_events.csv"
+CHUNK_SIZE = 5000
+STREAM_DELAY = 0.001
+
 producer = KafkaProducer(
-    bootstrap_servers="localhost:9092",
-    value_serializer=lambda v: json.dumps(v).encode("utf-8")
+    bootstrap_servers=KAFKA_BROKERS,
+    value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+    acks="all",
+    retries=5,
+    linger_ms=10,
+    batch_size=16384,
+    enable_idempotence=True
 )
 
-df = pd.read_csv("../../data/raw/ecommerce_events.csv")
+print("🚀 Producer Started...")
 
-# Treat large IDs as strings
-id_cols = ["order_id", "product_id", "category_id", "user_id"]
-for col in id_cols:
-    df[col] = df[col].astype(str)
+for chunk in pd.read_csv(FILE_PATH, chunksize=CHUNK_SIZE):
 
-for _, row in df.iterrows():
-    event = {
-        "event_time": row["event_time"],
-        "order_id": row["order_id"],
-        "product_id": row["product_id"],
-        "category_id": row["category_id"],
-        "category_code": row["category_code"],
-        "brand": row["brand"],
-        "price": float(row["price"]),
-        "user_id": row["user_id"]
-    }
-    producer.send("clickpulse_events", event)
-    time.sleep(0.002)   # adjust speed here
+    id_cols = ["order_id", "product_id", "category_id", "user_id"]
+    for col in id_cols:
+        chunk[col] = chunk[col].astype(str)
+
+    chunk["price"] = chunk["price"].astype(float)
+    records = chunk.to_dict(orient="records")
+
+    for event in records:
+        producer.send(TOPIC, key=event["user_id"].encode("utf-8"), value=event)
+        time.sleep(STREAM_DELAY)
+
+    print(f"Sent {len(records)} records...")
 
 producer.flush()
+producer.close()
+print("✅ Streaming Completed.")
